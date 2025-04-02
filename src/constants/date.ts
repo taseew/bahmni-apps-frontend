79e0cc8ac60fb6de8bfbbb5a956b3e88faebe3dd
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-04-02 14:45:45 +0530

    BN-47 | Fix. Default Date Format
    (cherry picked from commit d82704a1bc6314461d2aacf736846b6c13c22f17)

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,2 +1,2 @@
-export const DATE_FORMAT = 'dd-mm-yyyy';
-export const DATE_TIME_FORMAT = 'dd-mm-yyyy hh:mm';
+export const DATE_FORMAT = 'dd/MM/yyyy';
+export const DATE_TIME_FORMAT = 'dd/MM/yyyy HH:mm';
